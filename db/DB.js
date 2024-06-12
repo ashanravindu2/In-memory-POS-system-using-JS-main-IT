dc3470c138b3cf82f7d363d5b0b6e113bbebb29a
ashanravindu2 <ravindulakshithaashan@gmail.com>
2024-06-13 00:39:13 +0530

    Done%

---
+++ db/DB.js
@@ -0,0 +1,30 @@
+export let Customers = [
+    {
+        custId : 'C01',
+        custName : 'Ravindu',
+        custAddress : 'Colombo',
+        custSalary : '2000'
+    } ,
+
+    {
+        custId : 'C02',
+        custName : 'Kasun',
+        custAddress : 'Galle',
+        custSalary : '1000'
+    }
+];
+export let Items = [
+    {
+        itemId : 'I01',
+        itemName : 'Biscuit',
+        itemQty : 100,
+        itemPrice : 200
+    },
+    {
+        itemId : 'I02',
+        itemName : 'Necto',
+        itemQty : 120,
+        itemPrice : 150
+    }
+];
+export let Orders = [];
